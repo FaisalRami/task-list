fe2ef531cc453eb95b8668e6debd288a4e308138
FaisalRami <faisalramyy123@gmail.com>
2025-02-21 02:46:55 +0200

    third commit

--- routes/web.php
+++ routes/web.php
@@ -33,11 +33,29 @@ Route::post('/about' , function(){
 
 
 Route::get('tasks' , function(){
-    return view('tasks');
+    $tasks = DB::table('tasks')->get();
+    return view('tasks' , compact('tasks'));
 });
 
 Route::POST('create' , function(){
     $task_name = $_POST['name'];
     DB::table('tasks')->insert(['name'=>$task_name]);
-    return view('tasks');
+    return redirect()->back();
+});
+
+Route::POST('delete/{id}' , function($id){
+    DB::table('tasks')->where('id' , $id)->delete();
+    return redirect()->back();
+});
+
+Route::POST('edit/{id}' , function($id){
+    $task = DB::table('tasks')->where('id' , $id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks' , compact( 'task' ,'tasks'));
+});
+
+Route::POST('update' , function(){
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id' , $id)->update(['name'=>$_POST['name']]);
+    return redirect('tasks');
 });
