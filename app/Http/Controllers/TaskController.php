837bfac0ab187ba263b306f342734db19aa37ecd
FaisalRami <faisalramyy123@gmail.com>
2025-03-16 09:06:41 +0200

    fifth commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,33 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
 
     public function index(){
-        $tasks = DB::table('tasks')->get();
+        //$tasks = DB::table('tasks')->get();
+        $tasks=Task::all();
         return view('tasks' , compact('tasks'));
     }
 
     public function create(){
         $task_name = $_POST['name'];
-        DB::table('tasks')->insert(['name'=>$task_name]);
+        //DB::table('tasks')->insert(['name'=>$task_name]);
+        $task=new Task;
+        $task->name=$task_name;
+        $task->save();
         return redirect()->back();
     }
 
     public function destroy($id){
-        DB::table('tasks')->where('id' , $id)->delete();
+        //DB::table('tasks')->where('id' , $id)->delete();
+        Task::find($id)->delete();
         return redirect()->back();
     }
     public function edit($id){
-        $task = DB::table('tasks')->where('id' , $id)->first();
-        $tasks = DB::table('tasks')->get();
+        //$task = DB::table('tasks')->where('id' , $id)->first();
+        //$tasks = DB::table('tasks')->get();
+        $task=Task::find($id);
+        $tasks=Task::all();
         return view('tasks' , compact( 'task' ,'tasks'));
     }
     public function update(){
-        $id = $_POST['id'];
-        DB::table('tasks')->where('id' , $id)->update(['name'=>$_POST['name']]);
+        $id = $_REQUEST['id'];
+        Task::where('id' , $id)->update(['name'=>$_REQUEST['name']]);
         return redirect('tasks');
     }
 
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,38 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class UserController extends Controller
 {
 
     public function index(){
-        $users = DB::table('users')->get();
-    return view('users' , compact('users'));
+        // $users = DB::table('users')->get();
+        $users=User::all();
+        return view('users' , compact('users'));
     }
 
     public function adduser(){
         $user_name = $_POST['name'];
         $user_email = $_POST['email'];
         $user_password = $_POST['password'];
-        DB::table('users')->insert(['name'=>$user_name , 'email'=>$user_email , 'password'=>$user_password]);
+        // DB::table('users')->insert(['name'=>$user_name , 'email'=>$user_email , 'password'=>$user_password]);
+        $user=new User;
+        $user->name=$user_name;
+        $user->email=$user_email;
+        $user->password=$user_password;
+        $user->save();
         return redirect()->back();
     }
 
     public function destroyuser($id){
-        DB::table('users')->where('id' , $id)->delete();
+        // DB::table('users')->where('id' , $id)->delete();
+        User::find($id)->delete();
         return redirect()->back();
     }
 
     public function edituser($id){
-        $user = DB::table('users')->where('id' , $id)->first();
-        $users = DB::table('users')->get();
+        // $user = DB::table('users')->where('id' , $id)->first();
+        // $users = DB::table('users')->get();
+        $user=User::find($id);
+        $users=User::all();
         return view('users' , compact( 'user' ,'users'));
     }
 
     public function updateuser(){
-        $id = $_POST['id'];
-        DB::table('users')->where('id' , $id)->update(['name'=>$_POST['name'], 'email'=>$_POST['email'], 'password'=>$_POST['password']]);
+        $id = $_REQUEST['id'];
+        // DB::table('users')->where('id' , $id)->update(['name'=>$_POST['name'], 'email'=>$_POST['email'], 'password'=>$_POST['password']]);
+        User::where('id' , $id)->update(['name'=>$_REQUEST['name'], 'email'=>$_REQUEST['email'], 'password'=>$_REQUEST['password']]);
         return redirect('users');
     }
-    
+
 }
